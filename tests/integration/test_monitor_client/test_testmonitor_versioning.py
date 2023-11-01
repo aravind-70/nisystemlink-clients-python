e878909d6a645ad7347beb2c358cd8ca2ad592ea
Aravind K. Murali <aravind.murali@solitontech.com>
2023-11-01 19:54:25 +0530

    feat: add code for review

--- tests/integration/test_monitor_client/test_testmonitor_versioning.py
+++ tests/integration/test_monitor_client/test_testmonitor_versioning.py
@@ -1,22 +1,21 @@
-"""This file contains test functions for the versioning API of TestMonitorClient."""
+"""This file contains test function for the versioning API of TestMonitorClient."""
 # Third Party Modules
 import pytest
 # Relative modules
 from nisystemlink.clients.testmonitor import TestMonitorClient
- 
- 
+
 @pytest.fixture(scope="class")
 def client(enterprise_config):
     """Fixture to create a TestMonitorClient instance."""
     return TestMonitorClient(enterprise_config)
- 
- 
+
+
 @pytest.mark.enterprise
 @pytest.mark.integration
 class TestSuiteTestMonitorClientVersioning:
-    """Class contains a test methods to test versioning API of TestMonitor."""
- 
-    def test__api_info(self, client):
-        """Test second version of api info."""
+    """Class contains a test method to test versioning API of TestMonitor."""
+
+    def test__api_info(self, client: TestMonitorClient):
+        """Test the versioning API info."""
         response = client.api_info()
         assert len(response.dict()) != 0
